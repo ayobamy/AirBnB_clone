175cae661365b96a9a731aa8ec8c89f61648208f
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-08-07 18:49:21 +0000

    [feat]: added modules for the init file

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,12 @@ The __init__ method for models package
 """
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
 
 
 storage = FileStorage()
